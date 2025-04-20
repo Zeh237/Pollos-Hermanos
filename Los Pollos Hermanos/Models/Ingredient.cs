cd5c8b7f8728ff470c6a956aa71509bd33ad9bd0
Zeh237 <zehbrien@gmail.com>
2025-04-20 11:07:56 +0100

    updated products table with imageurl

--- Los Pollos Hermanos/Models/Ingredient.cs
+++ Los Pollos Hermanos/Models/Ingredient.cs
@@ -1,7 +1,11 @@
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
 namespace Los_Pollos_Hermanos.Models{
     public class Ingredient{
         public int Id {get; set;}
         public string Name {get; set;}
+
+        [ValidateNever]
         public ICollection<ProductIngredient> ProductIngredients {get; set;}
     }
 }
--- Los Pollos Hermanos/Models/Product.cs
+++ Los Pollos Hermanos/Models/Product.cs
@@ -1,3 +1,6 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
 namespace Los_Pollos_Hermanos.Models{
     public class Product{
         public int Id {get; set;}
@@ -6,8 +9,18 @@ namespace Los_Pollos_Hermanos.Models{
         public decimal price {get; set;}
         public int stock {get; set;}
         public int CategoryId {get; set;}
+
+        [NotMapped]
+        public IFormFile ImageFile {get; set;}
+        public string ImageUrl {get; set; } = "https://via.placeholder.com/159";
+
+        [ValidateNever]
         public Category? Category {get; set;}
+
+        [ValidateNever]
         public ICollection<OrderItem>? OrderItems {get; set;}
+
+        [ValidateNever]
         public ICollection<ProductIngredient>? ProductIngredients {get; set;}
 
     }
